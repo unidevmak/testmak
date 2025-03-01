67772fec857fc0bc472cf3f5aa29c843b7b42f8e
unidevmak <unidevmak@gmail.com>
2025-03-01 21:41:34 +0530

    Update testing.js

--- testing.js
+++ testing.js
@@ -10,3 +10,17 @@ a
 test('Intentional failure for SonarCloud', () => {
   expect(1 + 1).toBe(3); // This will fail
 });
+
+
+test('Intentional failure for SonarCloud', () => {
+  expect(1 + 1).toBe(3); // This will fail
+});
+
+// NOSONAR
+public void someMethod() {
+    // Method implementation
+a
+}
+test('Intentional failure for SonarCloud', () => {
+  expect(1 + 1).toBe(3); // This will fail
+});
