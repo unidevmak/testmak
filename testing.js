fa8663b0b4864a4e6ae46e010d13ac977a2a75a7
unidevmak <unidevmak@gmail.com>
2025-03-01 21:38:36 +0530

    Update and rename testing to testing.js

---
+++ testing.js
@@ -0,0 +1,12 @@
+test('Intentional failure for SonarCloud', () => {
+  expect(1 + 1).toBe(3); // This will fail
+});
+
+// NOSONAR
+public void someMethod() {
+    // Method implementation
+a
+}
+test('Intentional failure for SonarCloud', () => {
+  expect(1 + 1).toBe(3); // This will fail
+});
