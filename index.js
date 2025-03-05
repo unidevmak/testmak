de20332e9e7b2132377f48c0b069bf9fd3af54f9
unidevmak <unidevmak@gmail.com>
2025-03-05 11:23:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   const userInput = req.query.input;
   // Intentional Bug: Using eval() can lead to code injection vulnerabilities.
-  let result = eval("2 + " + userInput); 
-  res.send(`Result is: ${result}`);
+  // let result = eval("2 + " + userInput); 
+  res.send(`Result is: true`);
 });
 
 // Unused variable example (dead code)
-const unusedVariable = 42;
+// const unusedVariable = 42;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
